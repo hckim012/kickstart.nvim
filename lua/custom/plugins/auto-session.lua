833f45ebeb57f62c728857e98345b69573242237
Heechang Kim <0101khc@gmail.com>
2025-06-21 14:44:29 +0000

    Add auto-session as custom plugin

---
+++ lua/custom/plugins/auto-session.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    -- log_level = 'debug',
+  },
+}
